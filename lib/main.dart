c553f54f36aaa09957c0133022b352fea53a9ca8
sandidn <robertvanzelo@gmail.com>
2023-06-15 16:11:20 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> dataList = [];
+
+  Future<void> fetchData() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/albums');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      setState(() {
+        dataList = jsonDecode(response.body);
+      });
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HTTP Request - 201011401070'),
+      ),
+      body: ListView.builder(
+        itemCount: dataList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(dataList[index]['title']),
+          );
+        },
+      ),
+    );
+  }
+}
